e0e096ebbb8398f50f44d78c7c06ec0cb2e7980f
Chao Liang <52976469+Chao0813@users.noreply.github.com>
2019-07-23 15:01:40 -0400

    Add files via upload

---
+++ Nasa_api.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+import urllib.request
+import json
+
+
+
+app = Flask(__name__)
+
+
+@app.route("/home", methods=['POST'])
+def home():
+	apodurl = 'https://api.nasa.gov/planetary/apod?api_key='
+	mykey = request.form['api']
+
+	apodurlobj = urllib.request.urlopen(apodurl + mykey)
+
+	apodread = apodurlobj.read()
+
+	decodeapod = json.loads(apodread.decode('utf-8'))
+
+	image= decodeapod['apodurl']
+
+	return render_template('layout.html', picture=image)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+  
+
+
